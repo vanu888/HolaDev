dc0926ab26e9e32c97db5fae1b36219a30aa542f
vanu888 <173596364+vanu888@users.noreply.github.com>
2026-01-03 08:24:02 +0530

    Update app to Version 2

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return f"Hello from the DevOps Container! Host: {os.uname()[1]}"
+    # Change the text below:
+    return f"🚀 SUCCESS! Version 2 is running on {os.uname()[1]}"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
